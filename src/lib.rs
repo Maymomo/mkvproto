9bbf12fe6a93d65b0497402355901055cca085bb
Maymomo <3466365807@qq.com>
2019-10-19 10:49:17 +0800

    demo

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#[allow(clippy::all)]
+
+mod protos {
+    include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
+}
+
+
+pub use protos::*;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_demo() {
+        let mut get = demo::RequestGet::new();
+        assert_eq!("", get.get_key())
+    }
+}
